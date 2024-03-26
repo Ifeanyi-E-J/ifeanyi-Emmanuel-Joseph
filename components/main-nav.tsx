2d34f6080a49b25c48ff83f63bed1bc761ce0fa1
the2MinEngineer <ifeanyiemmanueljoseph@gmail.com>
2024-03-26 02:22:24 +0100

    stage 3 site header

---
+++ components/main-nav.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import { Icons } from "./Icons";
+import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+
+const MainNav = () => {
+	const pathname = usePathname();
+	return (
+		<nav className="flex items-center space-x-4 lg:space-x-6">
+			<Link
+				href="/"
+				className="mr-6 flex items-center space-x-2"
+			>
+				<Icons.logo className="h-6 w-6" />
+				<span className="font-bold">{siteConfig.name}</span>
+			</Link>
+			<Link
+				href="/blog"
+				className={cn(
+					"text-sm font-medium transition-colors hover:text-primary",
+					pathname === "./blog" ? "text-foreground" : "text-foreground/60"
+				)}
+			>
+				Blog
+			</Link>
+		</nav>
+	);
+};
+
+export default MainNav;
---
+++ config/site.ts
@@ -0,0 +1,15 @@
+export const siteConfig = {
+	name: "CaptainCode",
+	url: "https://example.com",
+	description: "Nextjs 14 Blog using velite, tailwind and shadcn",
+	author: "Ifeanyi Emmanuel",
+	links: {
+		twitter: "",
+		github: "https://github.com/The2MinEngineer",
+		Resume:
+			"https://drive.google.com/file/d/1MNkzyMeeSI2_3Otk37vUt4u6j2UchcOL/view?usp=drive_link",
+		linkedIn: "https://www.linkedin.com/in/ifeanyi-emmanuel/",
+	},
+};
+
+export type SiteConfig = typeof siteConfig;
